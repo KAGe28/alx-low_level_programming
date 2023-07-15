1c15bbca5c98c44a8cc621b8cc09a493aa81ac91
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-15 16:03:22 +0000

    Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";	
 	fwrite(s, strlen(s), 1, stderr);
-	fwrite('\n', 1, 1, stderr);
+	fwrite("\n", 1, 1, stderr);
 	return (1);
 }
