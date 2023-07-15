5049958500bce29b6e9354da3a368565e9628523
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-15 15:53:25 +0000

    Fix betty-coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
   * main - Entry point
@@ -7,6 +8,7 @@
   */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59, 1, stderr);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fwrite(s"\n", strlen(s), 1, stderr);
 	return (1);
 }
