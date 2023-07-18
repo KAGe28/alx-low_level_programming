0f2990d0ffba00503bbd6772f8436ad89f13e7fc
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-18 00:05:44 +0000

    Add task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Succes)
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+
+	return (0);
+}
