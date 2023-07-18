bf000cbaf5222b17bcb62a615621c7d93040a189
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-18 14:48:34 +0000

    Add task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+  * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+  *
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n < 10; n++)
+		for (i = 97; i <= 122; i++)
+			_putchar(i);
+	_putchar('\n');
+}
