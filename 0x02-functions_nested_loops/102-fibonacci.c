a29b1dbf786f7f9e9dc8d6fe8d6a3650176045b6
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-19 19:48:49 +0000

    Fix error

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	long int n = 50;
 	long int prev = 1;
 	long int forw = 2;
-	long int next;
+	long int next = 0;
 	int i;
 	
 	printf("%ld, ", prev);
@@ -21,12 +21,14 @@ int main(void)
 
 	for (i = 0; i < n; i++)
 	{
-		fibo = prev + forw;
+		next = prev + forw;
 		prev = forw;
-		forw = fibo;
-		printf("%ld", fibo);
-		if (i < (term - 1))
+		forw = next;
+		printf("%ld", next);
+		if (i < (n - 1))
 			printf(", ");
 	}
 	printf("\n");
+
+	return (0);
 }
