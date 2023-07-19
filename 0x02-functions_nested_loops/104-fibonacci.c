d329256cb2802b91461c3d2dcb61dde3f57f34cf
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-19 20:20:50 +0000

    Edit format for unsigned

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,15 +16,15 @@ int main(void)
 	unsigned int next = 0;
 	int i;
 
-	printf("%ld, ", prev);
-	printf("%ld, ", forw);
+	printf("%lu, ", prev);
+	printf("%lu, ", forw);
 
 	for (i = 2; i < n; i++)
 	{
 		next = prev + forw;
 		prev = forw;
 		forw = next;
-		printf("%ld", next);
+		printf("%lu", next);
 		if (i < (n - 1))
 			printf(", ");
 	}
