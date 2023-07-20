81736e7edf0d8a1e9c2b2e180160e3b003a722a7
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-20 02:45:45 +0000

    Fix output

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 	{
-		_putchar('\n');
+		_putchar('\0');
 	}
 	else
 	{
@@ -42,6 +42,12 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(t + '0');
+				}
+				else if (t > 10 && t < 100)
+				{
+					_putchar(' ');
+					_putchar((t / 10 % 10) + '0');
+					_putchar((t % 10) + '0');
 				}
 				else
 				{
