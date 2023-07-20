372ef085957cf875e9bb4a6475e34d81f610f5d8
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-20 01:54:46 +0000

    Fix task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 
 /* betty style doc for function main goes there */
 /**
-  * times_tables - prints the 9 times table, starting with 0
+  * times_table - prints the 9 times table, starting with 0
   *
   * Return: void
   */
