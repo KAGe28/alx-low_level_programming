48e48b2f3a8afe93f1afa84102e57b302d19d992
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-22 00:53:23 +0000

    Fix loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,7 +20,7 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = size - 1; j <= i; j--)
+			for (j = size - 1; j >= i; j--)
 				_putchar(' ');
 			for (k = 1; k <= 1; k++)
 				_putchar('#');
