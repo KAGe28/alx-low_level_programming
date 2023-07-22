2d98a4ca9674ad09382947f4e45a201bdb8ca950
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-07-22 01:06:21 +0000

    Fix inner character loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -21,9 +21,13 @@ void print_triangle(int size)
 		for (i = 1; i <= size; i++)
 		{
 			for (j = size - 1; j >= i; j--)
+			{
 				_putchar(' ');
-			for (k = 1; k <= 1; k++)
+			}
+			for (k = 1; k <= i; k++)
+			{
 				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
