643ca1679d3cdef78337258578feb7eb3937ea3b
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-01 21:55:37 +0000

    Add task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,41 +1,24 @@
 #include "main.h"
 
 /**
-  * _strlen - returns the length of a string
-  * @s: a pointer to a char
-  * Return: length of s
-  */
-
-int _strlen(char *s)
-{
-	int i;
-	int l;
-
-	for (i = 0; s[i] != '\0'; i++)
-		l += 1;
-	return (l);
-}
-
-/**
-  * print_chessboard - Locates a character in a string
-  * @a: a pointer to a char
+  * print_diagsums - Prints the sum of the 2 diagonals
+  * @a: a pointer to int
+  * @size: size of array
   * Return: nothing
   */
 
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
 	int i;
-	int j;
-	int l1;
-	int l2;
+	int s1 = 0;
+	int s2 = 0;
+	int *p1 = a;
+	int *p2 = a + size - 1;
 
-	l2 = sizeof(a[0]) / sizeof(a[0][0]);
-	l1 = l2;
-
-	for (i = 0; i < l1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < l2; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		s1 += *(p1 + i * size + i);
+		s2 += *(p2 + i * size - i);
 	}
+	printf("%d, %d\n", s1, s2);
 }
