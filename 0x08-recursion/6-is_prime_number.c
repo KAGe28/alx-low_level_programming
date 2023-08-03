f27e0e5fcc7b80589931b0813edef9ac4b028d15
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-03 01:46:40 +0000

    Fix Segmentation fault

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,22 +4,18 @@
   * check_prime - Checks for prime number
   * @n: the number to check
   * @i: The counter for  loop
-  * @prime: count for checking prime
   * Return: 1 if n is prime, 0 otherwise
   */
 
-int check_prime(int n, int i, int prime)
+int check_prime(int n, int i)
 {
-	/*printf("prime = %d\ni = %d\n", prime, i);*/
-
-	if (i > n && prime > 2)
+	if (n <= 1)
 		return (0);
-
-	if (prime == 2 && i == n)
+	if (i >= n)
 		return (1);
 	if (n % i == 0)
-		prime++;
-	return (check_prime(n, i + 1, prime));
+		return (0);
+	return (check_prime(n, i + 1));
 }
 
 /**
@@ -30,10 +26,5 @@ int check_prime(int n, int i, int prime)
 
 int is_prime_number(int n)
 {
-	if (n < 0)
-		return (0);
-	if (n == 1)
-		return (0);
-
-	return (check_prime(n, 1, 0));
+	return (check_prime(n, 2));
 }
