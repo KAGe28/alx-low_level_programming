436b10a000c47a80b56226d85cab4879d189e3b0
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-07 08:00:12 +0000

    Fix correct output

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,11 +13,17 @@ int main(int argc, char *argv[])
 	int cents = 0;
 	int n = 0;
 
-	if (argc != 2 || atoi(argv[1]) < 0)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
 	else
 	{
 		n = atoi(argv[1]);
