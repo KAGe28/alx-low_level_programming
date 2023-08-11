d50024f3afb9a659b3a893aeb31cf2425b084fff
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-11 20:43:55 +0000

    Fix the good amount of memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if ((int)n >= l2)
 		n = l2;
-	concat = malloc(sizeof(char) * (l1 + l2 + 1));
+	concat = malloc(sizeof(char) * (l1 + n + 1));
 
 	if (concat == NULL)
 		return (NULL);
