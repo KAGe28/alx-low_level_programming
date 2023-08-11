b2d364ff0fbd2827475da1f1364878b6fa609727
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-11 16:21:25 +0000

    Add task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+  * _strlen - returns the length of a string
+  * @s: a pointer to a char
+  * Return: length of s
+  */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+	return (l);
+}
+
+/**
+  * string_nconcat - concatenantes two string
+  * @s1: a pointer to the first string
+  * @s2: a pointer to the second
+  * @n:number of bytes
+  * Return: a pointer to a new string (s1 + s2) + '\0'
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, l1, l2;
+	char *concat = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	concat = malloc(sizeof(char) * (l1 + l2 + 1));
+
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		concat[i] = s1[i];
+	if ((int)n >= l2)
+	{
+		for (j = 0, i = l1; s2[j] != '\0'; j++, i++)
+			concat[i] = s2[j];
+	}
+	else
+	{
+		for (j = 0, i = l1; j < (int)n && s2[j] != '\0'; j++, i++)
+			concat[i] = s2[j];
+	}
+
+	return (concat);
+}
