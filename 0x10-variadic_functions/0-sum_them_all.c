301f78bad15b9bfddce282660a2e9d71f6a1ceaa
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-18 11:00:26 +0000

    Fix directory name

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+  * sum_them_all - returns the sum of all parameters
+  * @n: the parameter(s)
+  * Return: sum of n
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0;
+	int i = 0;
+
+	if (n == 0)
+		return (0);
+	va_list args;
+	va_start(args,(int)n);
+	
+	for (i = 0; i < (int)n; i++)
+		s += va_arg(args, int);
+	va_end(args);
+
+	return (s);
+}
