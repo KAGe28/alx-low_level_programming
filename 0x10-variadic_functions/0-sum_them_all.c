0d651a6dba5c474eda93138720a071e0ea6efcd2
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-18 11:11:17 +0000

    Fix ISO C89

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int s = 0;
 	int i = 0;
+	va_list args;
 
 	if (n == 0)
 		return (0);
-	va_list args;
-	va_start(args,(int)n);
-	
+	va_start(args, (int)n);
+
 	for (i = 0; i < (int)n; i++)
 		s += va_arg(args, int);
 	va_end(args);
