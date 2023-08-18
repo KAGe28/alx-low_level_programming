06dc462f252b8b586f2ee9a9e541dcc38b8b288d
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-18 11:02:56 +0000

    Fix compile program

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,12 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+#include <stdarg.h>
 #ifndef NULL
 #define NULL ((void*)0)
 #endif
 
 int _putchar(char c);
 int _strlen(char *s);
-
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /* MAIN_H */
