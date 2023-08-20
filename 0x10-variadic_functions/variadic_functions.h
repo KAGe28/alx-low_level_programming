1d8b2f0662b5f675fff93fb986220579c7b78c96
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-08-20 17:43:51 +0000

    Add and fix task 1 and 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,11 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 #ifndef NULL
-#define NULL ((void*)0)
+#define NULL ((void *)0)
 #endif
 
 int _putchar(char c);
 int _strlen(char *s);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif /* MAIN_H */
