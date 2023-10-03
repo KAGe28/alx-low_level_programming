dc20a67906019b8db3fae9584a42a675963aa0fe
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-10-03 02:36:32 +0100

    Fix task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,10 +13,18 @@ int main(int argc, char *argv[])
 	int count = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2 || !isdigit(*argv[1]))
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	for (i = 0; argv[1][i] != '\0'; i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
 	num = atoi(argv[1]);
 
