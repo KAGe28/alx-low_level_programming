34913f134c66beb5826877b1420d436de2c27185
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-10-10 16:14:46 +0100

    Add tasks 0 1 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+  * init_dog - Initialize a variable of type struct dog
+  * @d: a dog
+  * @name: name of dog
+  * @age: age of the dog
+  * @owner: owner's name of the dog
+  * Return: Nothing
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+/**
+  * struct dog - define a dog
+  * @name: Name of dog
+  * @age: Age of dog
+  * @owner: Owner's name of dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
