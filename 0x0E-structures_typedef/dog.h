c30aa403ca24190a2833713ee40e6711a4520d4a
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-10-10 17:04:13 +0100

    Add prototype in header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,7 @@ struct dog
 
 typedef struct dog dog_t;
 
+char *_strcpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
