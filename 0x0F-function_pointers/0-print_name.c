9aef62efa008bb1569966fed8d9b4ec6b15cacee
Arnaud Geraldin KABA <arnaudgeraldinkaba@gmail.com>
2023-10-12 01:40:03 +0100

    Creates folder and add tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+  * print_name- Prints a name
+  * @name: Name passed
+  * @f: The function pointed
+  * Return: Nothing
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(f)(name);
+}
+
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator- Executes a function given as a parameter
+  * @array: Array passed
+  * @size: The size of the array
+  * @action: a pointer to function you need to use
+  * Return: Nothing
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(action)(array[i]);
+}
+
